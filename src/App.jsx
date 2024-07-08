66408e975cb3bdd9e406cb07e4e82e09778fd62c
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-07-08 19:46:31 +0700

    Link vid

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
         <p className='font-bold mb-10 text-[25px] text-center'><span className='text-[#00426D]'>OPENING KKN TEMATIK 2024</span><br></br><span className='text-[#00426D]'>POLITEKNIK LP3I KAMPUS TASIKMALAYA</span></p>
         <Link to={`/video`} className='flex items-center justify-center'>
           <img src={handLogo} alt=""/>
-          <span>Pindah</span>
         </Link>
         <div className='flex gap-5 mt-10 items-center justify-center'>
           <div className='w-56'><img src={lp3iLogo} alt=""/></div>
