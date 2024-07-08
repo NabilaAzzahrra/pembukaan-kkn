f80412085d63f22373b2674865e03c658114d024
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-07-08 16:54:18 +0700

    get video

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import handLogo from '/HIHI.png'
+import lp3iLogo from '/lp3i.png'
+import ugmLogo from '/ugm.png'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <div>
-        <a href="http://">
-        <img src={handLogo} alt=""/></a>
+        <p className='font-bold mb-10 text-[25px] text-center'><span className='text-[#00426D]'>OPENING KKN TEMATIK 2024</span><br></br><span className='text-[#00426D]'>POLITEKNIK LP3I KAMPUS TASIKMALAYA</span></p>
+        <Link to={`/video`} className='flex items-center justify-center'>
+          <img src={handLogo} alt=""/>
+          <span>Pindah</span>
+        </Link>
+        <div className='flex gap-5 mt-10 items-center justify-center'>
+          <div className='w-56'><img src={lp3iLogo} alt=""/></div>
+          <div className='w-48'><img src={ugmLogo} alt=""/></div>
+        </div>
       </div>
     </>
   )
---
+++ src/Video.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Video = () => {
+  return (
+    <div>
+        <h1>Video</h1>
+        <Link to={`/`}>awal</Link>
+    </div>
+  )
+}
+
+export default Video
